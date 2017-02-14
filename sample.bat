40454cbcfefe2c96be26c865ba0ebc310029a94b
MoulikaB <moulikabalram@gmail.com>
2017-02-14 17:22:17 +0530

    Update sample.bat

--- sample.bat
+++ sample.bat
@@ -1,1 +1,1 @@
-copy C:\Users\moulika.balaram\Desktop\DemoProject\target\DemoProject-0.0.1-SNAPSHOT.war C:\Tools\apache-tomcat-8.0.37\webapps\Deploy\
+copy C:\Users\Vani_Alubilli\Downloads\newProject-master\newProject-master\target\DemoProject-0.0.1-SNAPSHOT.war C:\Tools\apache-tomcat-8.0.37\webapps\Deploy\
